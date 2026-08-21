08df9922a9f16a75ebf687feea42cd8bfe910522
Rafael Chiareli Jr <chiarelirafael@gmail.com>
2026-08-21 19:01:12 -0300

    first commit

---
+++ ClinicaASPNet/Controllers/PacientesController.cs
@@ -0,0 +1,89 @@
+﻿using ClinicaASPNet.Models;
+using ClinicaASPNet.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using System.IO.Pipelines;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ClinicaASPNet.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private readonly IPacienteRepository _repository;
+
+        public PacientesController(IPacienteRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public IActionResult Index(string? nome)
+        {
+            ViewBag.NomePesquisado = nome;
+            var pacientes = _repository.Listar();
+            return View(pacientes);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var paciente = _repository.BuscarPorId(id);
+            if (paciente is null) return NotFound();
+            return View(paciente);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Paciente paciente)
+        {
+            if (_repository.CpfJaCadastrado(paciente.Cpf))
+            {
+                ModelState.AddModelError(nameof(paciente.Cpf), "Ja existe um paciente" +
+                    "cadastrado com esse CPF");
+            }
+            if (ModelState.IsValid == false)
+            {
+                return View(paciente);
+            }
+            _repository.Adicionar(paciente);
+            TempData["MensagemSucesso"] = "Paciente cadastrado com sucesso";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var paciente = _repository.BuscarPorId(id);
+            if (paciente is null) return NotFound();
+            return View(paciente);  
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Paciente paciente, int id)
+        {
+            if (id != paciente.Id)
+            {
+                return BadRequest();
+            }
+            if (_repository.CpfJaCadastrado(paciente.Cpf))
+            {
+                ModelState.AddModelError(nameof(paciente.Cpf), "Ja existe um paciente" +
+                    "cadastrado com esse CPF");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(paciente);
+            }
+            if (!_repository.Atualizar(paciente))
+            {
+                return NotFound();
+            }
+              TempData["MensagemSucesso"] = "Paciente cadastrado com sucesso";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClinicaASPNet/Models/Paciente.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaASPNet.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatótio")]
+        [StringLength(100, MinimumLength =3, 
+            ErrorMessage = "O Nome deve possuir entre 3 e 100 Caracteres")]
+        [Display(Name = "Nome Completo")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Informe o cpf com 11 dígitos")]
+        public string Cpf { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Phone(ErrorMessage ="Informe um telefone válido")]
+        public string Telefone { get; set; } = string.Empty ;
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+
+    }
+}
